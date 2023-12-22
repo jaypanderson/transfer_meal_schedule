da0bc6e28ad2b2c2d7f17e5045bdb2cc35a1b9eb
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2023-12-22 10:13:56 +0900

    update function

--- transfer_meal_schedule.py
+++ transfer_meal_schedule.py
@@ -9,6 +9,7 @@ def open_excel_file() -> str:
 def extract_meal_data_big_kids(path: str) -> dict:
     book = openpyxl.load_workbook(path)
     sheet = book.active
+    date_ranges = find_date_ranges(sheet)
 
 
 def transfer_meal_schedule_big_kids():
