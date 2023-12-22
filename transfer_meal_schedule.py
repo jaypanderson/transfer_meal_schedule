c7c3b9076af1983137e764650948a70030f29ea2
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2023-12-22 10:02:08 +0900

    add function

--- transfer_meal_schedule.py
+++ transfer_meal_schedule.py
@@ -6,6 +6,10 @@ def open_excel_file() -> str:
     return filedialog.askopenfilename(title='献立表を選択してください。')
 
 
+def extract_meal_data_big_kids(path: str) -> dict:
+    pass
+
+
 def transfer_meal_schedule_big_kids():
     path = open_excel_file()
     meal_data = extract_meal_data_big_kids(path)
