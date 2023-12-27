14e8123ee8c2c580eb14b5fd5b7f3cb09524c997
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2023-12-27 11:00:25 +0900

    update

--- transfer_meal_schedule.py
+++ transfer_meal_schedule.py
@@ -7,6 +7,7 @@ from tkinter import filedialog
 from typing import Union
 from copy import copy
 from itertools import zip_longest
+import os
 
 
 def choose_file(file_type: int) -> str:
@@ -232,8 +233,11 @@ def copy_all_elements(sheet: Worksheet, new_sheet: Worksheet):
     copy_margins(sheet, new_sheet)
     copy_page_size(sheet, new_sheet)
 
+
 # todo add try except clause in case this cannot find the image, so that the program doesnt crash.
 def add_shapes(new_sheet: Worksheet):
+    print(os.curdir)
+    print(os.getcwd())
     image = Image('C:/Users/Ryan/Desktop/Work documents/smile kinder garden/事務書類/検食簿/image_boxes_test1.jpg')
     image.anchor = 'F1'
     new_sheet.add_image(image)
