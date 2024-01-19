c91c6da501af249eedb0e79255720903cd29e2be
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2024-01-19 09:27:18 +0900

    update docs

--- transfer_meal_schedule.py
+++ transfer_meal_schedule.py
@@ -229,9 +229,11 @@ def copy_page_size(sheet: Worksheet, new_sheet: Worksheet):
 # TODO finish docstring
 def copy_all_elements(sheet: Worksheet, new_sheet: Worksheet):
     """
-
-    :param sheet:
-    :param new_sheet:
+    A utility function to run other smaller functions. In openpyexl there is no straight forward way to completely copy
+    all aspects of a worksheet into another.  This requires accessing specific attributes that need to be copied.  In
+    the future if there are new attributes that need to be copied, create and add another function to this list.
+    :param sheet: original sheet the attributes will be copied from.
+    :param new_sheet: the new sheet where the attributes will be pasted into.
     :return:
     """
     copy_sheet(sheet, new_sheet)
