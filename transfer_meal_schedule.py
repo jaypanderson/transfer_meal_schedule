d4b427115d980c9ae48059ac6419807a9a57e11d
jaypanderson <134990416+jaypanderson@users.noreply.github.com>
2024-01-19 10:27:32 +0900

    add notes.

--- transfer_meal_schedule.py
+++ transfer_meal_schedule.py
@@ -389,7 +389,9 @@ def paste_meal_data(path: str, meal_data_big_kids: dict, meal_data_small_kids: d
         copy_all_elements(sheet, new_sheet)
         add_shapes(new_sheet)
         insert_data_big_kids(key, val_big, new_sheet)
-        if meal_data_small_kids and key in meal_data_small_kids: 
+        # checks to see if meal data is None and if the date exists in its dictionary. this can happen if the excell
+        # sheet contains errors that make the dates differ between the big and small kids.
+        if meal_data_small_kids and key in meal_data_small_kids:
             val_small = meal_data_small_kids[key]
             insert_data_small_kids(key, val_small, new_sheet)
 
